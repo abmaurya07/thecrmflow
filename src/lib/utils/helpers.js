26a9e00d75c7f5f394a60300065bb1f49e33ba72
lucifer-kaabil <abhishek@kaabil.me>
2024-10-05 18:24:56 +0530

    lsdkf

--- src/lib/utils/helpers.js
+++ src/lib/utils/helpers.js
@@ -57,21 +57,9 @@ async function downloadMedia(mediaId, mediaType) {
     headers: {
       'Authorization': `Bearer ${whatsappToken}`,
       'Content-Type': 'application/pdf'
-    }
-  })  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.arrayBuffer();  // This converts the body of the response
-  })
-  .then((buffer) => {
-    // Now you have the media as an ArrayBuffer
-    // You can convert this buffer to a Blob or other formats
-    console.log(buffer);
-  })
-  .catch((error) => {
-    console.error('There has been a problem with your fetch operation:', error);
-  })
+    },
+    responseType: 'arraybuffer',
+  })  
 
   
 console.log('mediaDownloadResponse', mediaDownloadResponse)
